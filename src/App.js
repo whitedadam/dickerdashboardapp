5b02e6a5f6a62664690f26e52b056aba1d803c04
Adam W <56056011+whitedadam@users.noreply.github.com>
2021-09-29 21:04:56 -0400

    Revert "Added simple login function"
    This reverts commit eb9c89dc208a588f13db248ac7de2ddcf2569db1.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
-import {Switch, Router, Route} from 'react-router-dom'
-import Login from "./Login";
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-      <div>
-        <Login/>
-      </div>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
