62a036b5a3da921ab4435cd4fcea98008f3080ec
Adam W <56056011+whitedadam@users.noreply.github.com>
2021-12-14 01:05:11 -0500

    Create AdminSettings.js

---
+++ src/AdminSettings/AdminSettings.js
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+import "./AdminSettings.css"
+import {Container, NavItem, NavLink, Table} from 'reactstrap';
+import adminData from "../mock-data.json"
+
+function AdminSettings() {
+    const [merch, setMerch] = useState(adminData);
+        return(
+            <Container>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>Merchant ID</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Phone Number</th>
+                            <th>Email</th>
+                            <th>App User ID</th>
+                            <th>Date Created</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {merch.map((merch)=> (
+                            <tr>
+                                <td>{merch.merchantId}</td>
+                                <td>{merch.firstName}</td>
+                                <td>{merch.lastName}</td>
+                                <td>{merch.phoneNumber}</td>
+                                <td>{merch.email}</td>
+                                <td>{merch.appUserId}</td>
+                                <td>{merch.dateCreated}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+                <NavItem className="adminDashNav">
+                    <NavLink href="/AdminDashboard">Admin Dashboard</NavLink>
+                </NavItem>
+             </Container>
+        );
+}
+
+export default AdminSettings;
