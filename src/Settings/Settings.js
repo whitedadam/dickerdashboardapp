134ddd9e9b19a02943957f777da605911ac65874
avsweet <aves.lsweet@gmail.com>
2021-12-14 11:21:04 -0500

    Update Settings.js
    Added Toggles

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -7,53 +7,59 @@ class Settings extends React.Component {
 
     constructor(props) {
         super(props);
-            this.state = {
-                data: null,
-                form: {
-                    email: '',
-                    password: ''},
+        this.state = {
+            data: null,
+            form: {
+                email: '',
+                password: ''
+            },
             userOption: "none",
             isGeneralSetting: false,
             isSecurity: false,
             isNotification: false,
             resetHidden: true,
-                users: {
-                    admin: {
-                        id: 1,
-                        email: 'admin@dicker.com',
-                        password: 'admin',
-                        name: 'Admin',
-                        phoneNumber: '(904)-456-3452' ,
-                        loginAttempts: 0,
-                        lockoutEnabled: false,
-                        isAdmin: true,
-                        allowEmail: "allowing",
-                        allowData: "not allowing"
-                    },
-                    merchant: {
-                        id: 2,
-                        email: 'firstcheck@lastcheck.test',
-                        password: '12345',
-                        name: 'Bob',
-                        phoneNumber: '(904)-675-6234' ,
-                        loginAttempts: 0,
-                        lockoutEnabled: false,
-                        isAdmin: false,
-                        allowEmail: "not allowing",
-                        allowData: "allowing"
+            isToggleOn: false,
+            isAllowDataOn: false,
+            users: {
+                admin: {
+                    id: 1,
+                    email: 'admin@dicker.com',
+                    password: 'admin',
+                    name: 'Admin',
+                    phoneNumber: '(904)-456-3452',
+                    loginAttempts: 0,
+                    lockoutEnabled: false,
+                    isAdmin: true,
+                    allowEmail: "allowing",
+                    allowData: "not allowing"
+                },
+                merchant: {
+                    id: 2,
+                    email: 'firstcheck@lastcheck.test',
+                    password: '12345',
+                    name: 'Bob',
+                    phoneNumber: '(904)-675-6234',
+                    loginAttempts: 0,
+                    lockoutEnabled: false,
+                    isAdmin: false,
+                    allowEmail: "not allowing",
+                    allowData: "allowing"
 
-                    }
                 }
+            }
         };
+
         this.handleClickGeneralSettings = this.handleClickGeneralSettings.bind(this);
         this.handleClickSecurity = this.handleClickSecurity.bind(this);
         this.handleClickNotification = this.handleClickNotification.bind(this);
+        this.handleClickAllowNotification = this.handleClickAllowNotification.bind(this);
+        this.handleClickAllowDatabase = this.handleClickAllowDatabase.bind(this);
     }
 
     handleClickGeneralSettings() {
         this.setState({userOption: 'General Setting', isGeneralSetting: true});
         window.alert('Name: ' + this.state.users.merchant.name + "\n" +
-            'Username: ' + this.state.users.merchant.email  + "\n" +
+            'Username: ' + this.state.users.merchant.email + "\n" +
             'Phone number: ' + this.state.users.merchant.phoneNumber);
     }
 
@@ -64,10 +70,24 @@ class Settings extends React.Component {
 
     handleClickNotification() {
         this.setState({userOption: 'Notification', isNotification: true});
-        window.alert( this.state.users.merchant.name + " is " + this.state.users.merchant.allowEmail + " emails to be sent." + "\n" +
-            this.state.users.merchant.name + " is " +  this.state.users.merchant.allowData + " data to be stored in Database." + "\n"
+        window.alert(this.state.users.merchant.name + " is " + this.state.users.merchant.allowEmail + " emails to be sent." + "\n" +
+            this.state.users.merchant.name + " is " + this.state.users.merchant.allowData + " data to be stored in Database." + "\n"
         );
     }
+
+    handleClickAllowNotification() {
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }));
+    }
+
+    handleClickAllowDatabase() {
+        this.setState(state => ({
+            isAllowDataOn: !state.isAllowDataOn
+        }));
+    }
+
+
 
 
 
@@ -91,6 +111,20 @@ class Settings extends React.Component {
                             <h3> Notification </h3>
                         </Button>
                     </Col>
+                <Col>
+                    <br/>
+                        <h4> Do you want to allow emails to be sent directly to you?</h4>
+                        <Button onClick={this.handleClickAllowNotification}>
+                            {this.state.isToggleOn ? "ON" : "OFF"}
+                        </Button>
+                    <br/>
+                </Col>
+                    <br/>
+                    <h5> Do you want to allow user information to be stored in Dicker Database?</h5>
+                    <Button onClick={this.handleClickAllowDatabase}>
+                        {this.state.isAllowDataOn ? "ON" : "OFF"}
+                    </Button>
+                <br/>
                 </Row>
                 <Row>
                     <Col> </Col>
@@ -101,6 +135,7 @@ class Settings extends React.Component {
                     </Col>
                     <Col> </Col>
                 </Row>
+
             </Container>
         );
     }
