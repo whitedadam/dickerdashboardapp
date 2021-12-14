17ccc41d2ea1966f2cda5f8820577411920a9aff
Adam W <56056011+whitedadam@users.noreply.github.com>
2021-12-13 23:49:59 -0500

    Update adminSettings.js

--- src/AdminSettings/adminSettings.js
+++ src/AdminSettings/adminSettings.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import {Col, Container, Row} from 'reactstrap';
+import React, {useState} from "react";
+import "./AdminSettings.css"
+import {Col, Container, Row, Table} from 'reactstrap';
+import adminData from "../mock-data.json"
 
-class AdminSettings extends React.Component {
-    render() {
-        return (
-            <Container className={'dashboardContainer'}>
-                <Row>
-                    <Col><h3>Dashboards sert</h3></Col>
-                </Row>
-                <Row>
-                </Row>
-            </Container>
+function AdminSettings() {
+    const [merch, setMerch] = useState(adminData);
+        return(
+            <Container>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>Merchant ID</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Phone Number</th>
+                            <th>Email</th>
+                            <th>App User ID</th>
+                            <th>Date Created</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {merch.map((merch)=> (
+                            <tr>
+                                <td>{merch.merchantId}</td>
+                                <td>{merch.firstName}</td>
+                                <td>{merch.lastName}</td>
+                                <td>{merch.phoneNumber}</td>
+                                <td>{merch.email}</td>
+                                <td>{merch.appUserId}</td>
+                                <td>{merch.dateCreated}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+             </Container>
         );
-    }
 }
 
 export default AdminSettings;
