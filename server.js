d05c841c2227a6a8772f024abfb30bc21b1c1234
whitedadam <whitedadam@yahoo.com>
2022-02-10 16:57:59 -0500

    Resolved API and Dynamic NavBar Blockers

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const Connection = require("tedious").Connection;
 const path = require("path");
-const users = require('./src/mock-data.json');
+const users = require("./src/mock-data.json");
 
 const config = {
   server: "codingwpride.database.windows.net",
@@ -32,48 +32,52 @@ connection.connect();
 
 const Request = require("tedious").Request;
 
-function executeStatement() {
-  let jsonArray = [];
+async function executeStatement(cb) {
+  // const jsonArray = [];
+
+  const newData = [];
+  const dataset = [];
+
   //select * from dbo.Business Test Statement
   let sql =
     "Select TOP (1000) [MerchantId],[FirstName],[LastName],[PhoneNumber],[Email],[AppUserId],[DateCreated] FROM [Merchant] WHERE MerchantId = 1";
-  let request = new Request(sql, function (err, rowCount) {
+  let request = new Request(sql, function (err, rowCount, rows) {
     if (err) {
       console.log(err);
     } else {
-      console.log(rowCount + " rows");
+      cb(rows, newData);
+      // console.log(rowCount + " rows");
     }
   });
 
   request.on("row", function (columns) {
     columns.forEach(function (column) {
       console.log(column.value);
-      jsonArray.push(column);
+      dataset.push({
+        [column.metadata.colName]: column.value,
+      });
     });
+    newData.push(dataset);
   });
 
   connection.execSql(request);
-
-  console.log(jsonArray);
 }
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
-app.get("/backend", (req, res) => {
-  let success = executeStatement();
-//   console.log(success);
-  //   res.send({ data: JSON.stringify(success) });
-  //   res.send('Boy, I wish I know how to use Express!');
-  res.json(success);
+app.get("/backend", async (req, res) => {
+  let success = await executeStatement((rows, data) => {
+    console.log(data + " rows");
+    res.json({
+      data: data.flat(),
+    });
+  });
+  console.log(success);
 });
 
 // create a POST route
-// app.get("/backend", (req, res) => {
-//   //   res.send({ data: JSON.stringify(success) });
-//     res.send(req.body);
-//   });
 
 // Sample of Sending JSON via Expresss
 app.get("/api/users", (req, res) => {
