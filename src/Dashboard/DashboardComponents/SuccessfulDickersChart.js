017d4b6c6dc22a6aca5c73c9c7e42455ab76d66c
whitedadam <whitedadam@yahoo.com>
2022-03-01 21:21:10 -0500

    Update SuccessfulDickersChart.js
    - Data filtering and drill down complete for this chart.
    - Just need to add handling for when aggregate division produces NaN

--- src/Dashboard/DashboardComponents/SuccessfulDickersChart.js
+++ src/Dashboard/DashboardComponents/SuccessfulDickersChart.js
@@ -8,31 +8,84 @@ import {
   DropdownMenu,
   DropdownToggle,
   Row,
+  Button,
+  Table,
 } from "reactstrap";
+import { TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 
 const SuccessfulDickersChart = ({ data: newData }) => {
   const today = new Date();
 
   // Filter Accepted Offer Data into YTD Offers
   const filterDataYTD = () => {
-    let totalDickers = 0;
+    // Date variables for filtering
     let pastDate = new Date(today);
     pastDate.setDate(pastDate.getDate() - 365);
 
-    // console.log(newData);
+    // Tracking Variables for Chart Data
+    let totalAcceptedDickers = 0;
+    let totalAcceptedWins = 0;
+
+    // InGrid DICKERs
+    let totalPotentialDickers = 0;
+    let totalPotentialDickersWins = 0;
+
+    // Wildcard DICKERs
+    let totalWildcardDickers = 0;
+    let totalWildcardWins = 0;
+
+    // Direct DICKERs
+    let totalDirectDickers = 0;
+    let totalDirectWins = 0;
+
+    // Discount
+    let avgDiscount = 0;
+
     try {
       newData.forEach((obj) => {
         const offerDate = new Date(obj.Created);
         if (pastDate <= offerDate) {
-          totalDickers++;
+          totalAcceptedDickers++;
+          if (obj.Win === true) {
+            totalAcceptedWins++;
+            avgDiscount += obj.Discount;
+          }
+          if (obj.InGrid === true) {
+            totalPotentialDickers++;
+            if (obj.Win === true) {
+              totalPotentialDickersWins++;
+            }
+          }
+          if (obj.Wildcard === true) {
+            totalWildcardDickers++;
+            if (obj.Win === true) {
+              totalWildcardWins++;
+            }
+          }
+          if (obj.DirectDICKER === true && obj.Win === true) {
+            totalDirectDickers++;
+            if (obj.Win === true) {
+              totalDirectWins++;
+            }
+          }
         }
       });
-      // console.log(newData);
+      avgDiscount = avgDiscount / totalAcceptedWins;
     } catch (err) {
       // console.log('err loading data');
     }
 
-    return totalDickers;
+    return [
+      totalAcceptedDickers, // 0
+      totalPotentialDickers, // 1
+      totalWildcardDickers, // 2
+      totalDirectDickers, // 3
+      totalAcceptedWins, // 4
+      totalPotentialDickersWins, // 5
+      totalWildcardWins, // 6
+      totalDirectWins, // 7
+      avgDiscount, // 8
+    ];
   };
 
   // Output Filtered YTD Data
@@ -40,22 +93,73 @@ const SuccessfulDickersChart = ({ data: newData }) => {
 
   // Filter Accepted Offer Data into YTD Offers
   const filterDataMonth = () => {
-    let totalDickers = 0;
+    // Date variables for filtering
     let pastDate = new Date(today);
     pastDate.setDate(pastDate.getDate() - 31);
+
+    // Tracking Variables for Chart Data
+    let totalAcceptedDickers = 0;
+    let totalAcceptedWins = 0;
+
+    // InGrid DICKERs
+    let totalPotentialDickers = 0;
+    let totalPotentialDickersWins = 0;
+
+    // Wildcard DICKERs
+    let totalWildcardDickers = 0;
+    let totalWildcardWins = 0;
+
+    // Direct DICKERs
+    let totalDirectDickers = 0;
+    let totalDirectWins = 0;
+
+    // Discount
+    let avgDiscount = 0;
 
     try {
       newData.forEach((obj) => {
         const offerDate = new Date(obj.Created);
         if (pastDate <= offerDate) {
-          totalDickers++;
+          totalAcceptedDickers++;
+          if (obj.Win === true) {
+            totalAcceptedWins++;
+            avgDiscount += obj.Discount;
+          }
+          if (obj.InGrid === true) {
+            totalPotentialDickers++;
+            if (obj.Win === true) {
+              totalPotentialDickersWins++;
+            }
+          }
+          if (obj.Wildcard === true) {
+            totalWildcardDickers++;
+            if (obj.Win === true) {
+              totalWildcardWins++;
+            }
+          }
+          if (obj.DirectDICKER === true && obj.Win === true) {
+            totalDirectDickers++;
+            if (obj.Win === true) {
+              totalDirectWins++;
+            }
+          }
         }
       });
     } catch (err) {
       // console.log('err loading data');
     }
 
-    return totalDickers;
+    return [
+      totalAcceptedDickers, // 0
+      totalPotentialDickers, // 1
+      totalWildcardDickers, // 2
+      totalDirectDickers, // 3
+      totalAcceptedWins, // 4
+      totalPotentialDickersWins, // 5
+      totalWildcardWins, // 6
+      totalDirectWins, // 7
+      avgDiscount, // 8
+    ];
   };
 
   // Output Filtered Month Data
@@ -63,22 +167,73 @@ const SuccessfulDickersChart = ({ data: newData }) => {
 
   // Filter Accepted Offer Data into Weekly Offers
   const filterDataWeek = () => {
-    let totalDickers = 0;
+    // Date variables for filtering
     let pastDate = new Date(today);
     pastDate.setDate(pastDate.getDate() - 7);
+
+    // Tracking Variables for Chart Data
+    let totalAcceptedDickers = 0;
+    let totalAcceptedWins = 0;
+
+    // InGrid DICKERs
+    let totalPotentialDickers = 0;
+    let totalPotentialDickersWins = 0;
+
+    // Wildcard DICKERs
+    let totalWildcardDickers = 0;
+    let totalWildcardWins = 0;
+
+    // Direct DICKERs
+    let totalDirectDickers = 0;
+    let totalDirectWins = 0;
+
+    // Discount
+    let avgDiscount = 0;
 
     try {
       newData.forEach((obj) => {
         const offerDate = new Date(obj.Created);
         if (pastDate <= offerDate) {
-          totalDickers++;
+          totalAcceptedDickers++;
+          if (obj.Win === true) {
+            totalAcceptedWins++;
+            avgDiscount += obj.Discount;
+          }
+          if (obj.InGrid === true) {
+            totalPotentialDickers++;
+            if (obj.Win === true) {
+              totalPotentialDickersWins++;
+            }
+          }
+          if (obj.Wildcard === true) {
+            totalWildcardDickers++;
+            if (obj.Win === true) {
+              totalWildcardWins++;
+            }
+          }
+          if (obj.DirectDICKER === true && obj.Win === true) {
+            totalDirectDickers++;
+            if (obj.Win === true) {
+              totalDirectWins++;
+            }
+          }
         }
       });
     } catch (err) {
       // console.log('err loading data');
     }
 
-    return totalDickers;
+    return [
+      totalAcceptedDickers, // 0
+      totalPotentialDickers, // 1
+      totalWildcardDickers, // 2
+      totalDirectDickers, // 3
+      totalAcceptedWins, // 4
+      totalPotentialDickersWins, // 5
+      totalWildcardWins, // 6
+      totalDirectWins, // 7
+      avgDiscount, // 8
+    ];
   };
 
   // Output Filtered Weekly Data
@@ -86,20 +241,69 @@ const SuccessfulDickersChart = ({ data: newData }) => {
 
   // Filter Accepted Offer Data into Todays Offers
   const filterDataToday = () => {
-    let totalDickers = 0;
+    // Tracking Variables for Chart Data
+    let totalAcceptedDickers = 0;
+    let totalAcceptedWins = 0;
+
+    // InGrid DICKERs
+    let totalPotentialDickers = 0;
+    let totalPotentialDickersWins = 0;
+
+    // Wildcard DICKERs
+    let totalWildcardDickers = 0;
+    let totalWildcardWins = 0;
+
+    // Direct DICKERs
+    let totalDirectDickers = 0;
+    let totalDirectWins = 0;
+
+    // Discount
+    let avgDiscount = 0;
 
     try {
-      newData.forEach((element) => {
-        const offerDate = new Date(element.Created);
+      newData.forEach((obj) => {
+        const offerDate = new Date(obj.Created);
         if (today.getDate() === offerDate.getDate()) {
-          totalDickers++;
+          totalAcceptedDickers++;
+          if (obj.Win === true) {
+            totalAcceptedWins++;
+            avgDiscount += obj.Discount;
+          }
+          if (obj.InGrid === true) {
+            totalPotentialDickers++;
+            if (obj.Win === true) {
+              totalPotentialDickersWins++;
+            }
+          }
+          if (obj.Wildcard === true) {
+            totalWildcardDickers++;
+            if (obj.Win === true) {
+              totalWildcardWins++;
+            }
+          }
+          if (obj.DirectDICKER === true && obj.Win === true) {
+            totalDirectDickers++;
+            if (obj.Win === true) {
+              totalDirectWins++;
+            }
+          }
         }
       });
     } catch (err) {
       // console.log('err loading data');
     }
 
-    return totalDickers;
+    return [
+      totalAcceptedDickers, // 0
+      totalPotentialDickers, // 1
+      totalWildcardDickers, // 2
+      totalDirectDickers, // 3
+      totalAcceptedWins, // 4
+      totalPotentialDickersWins, // 5
+      totalWildcardWins, // 6
+      totalDirectWins, // 7
+      avgDiscount, // 8
+    ];
   };
 
   // Output Filtered Today Data
@@ -150,6 +354,10 @@ const SuccessfulDickersChart = ({ data: newData }) => {
   );
 
   // console.log(displayData, displayData.deals);
+  const [drilldown, setDrilldown] = useState(false);
+  const handleDrilldown = () => {
+    setDrilldown(!drilldown);
+  };
 
   return newData === undefined ? (
     <div>Filtering Chart Data... </div>
@@ -161,12 +369,71 @@ const SuccessfulDickersChart = ({ data: newData }) => {
             <h5>{activeFilter.toUpperCase()}</h5>
           </Col>
           <Col lg={0}>
-            <p><strong>Accepted Total: </strong></p>
-            <p>{displayData.deals}</p>
+            <p>
+              <strong>Accepted Total: </strong>
+            </p>
+            <p>{displayData.deals[0]}</p>
+          </Col>
+          {/* <Col lg={0}>
+            <p><strong>DICKER Win % While InGrid: </strong></p>
+            <p>{displayData[1] < 0 ? 'No Data' : displayData.deals[1] / displayData.deals[0] * 100 + '%'}</p>
+          </Col> */}
+          <Col lg={0}>
+            <p>
+              <strong>Wildcard Wins: </strong>
+            </p>
+            <p>{displayData.deals[2]}</p>
+          </Col>
+          <Col lg={0}>
+            <p>
+              <strong>Direct DICKER Wins: </strong>
+            </p>
+            <p>{displayData.deals[3]}</p>
           </Col>
         </ResizableBox>
       </Row>
       <Row>
+        <Button onClick={handleDrilldown} color={"warning"}>
+          Drilldown
+        </Button>
+        {drilldown && (
+          <Row>
+            <h5>Accepted Dicker Drilldown</h5>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>DICKER Win % While InGrid</TableCell>
+                  <TableCell>Wildcard DICKER % of Total Wins</TableCell>
+                  <TableCell>Direct DICKER Wins % of Total Wins</TableCell>
+                  <TableCell>DICKER Win % Avg Discount</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    {displayData[1] < 0
+                      ? "No Data"
+                      : (displayData.deals[1] / displayData.deals[0]) * 100 +
+                        "%"}
+                  </TableCell>
+                  <TableCell>
+                    {displayData[4] < 0
+                      ? "No Data"
+                      : (displayData.deals[5] / displayData.deals[4]) * 100 +
+                        "%"}
+                  </TableCell>
+                  <TableCell>
+                    {displayData[4] < 0
+                      ? "No Data"
+                      : (displayData.deals[7] / displayData.deals[4]) * 100 +
+                        "%"}
+                  </TableCell>
+                  <TableCell>{displayData.deals[8] + "%"}</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </Row>
+        )}
         <Col>
           <ButtonDropdown
             isOpen={dropdownOpen}
