3209e4193f1e7fbbb262f479773b293e04883e5c
whitedadam <whitedadam@yahoo.com>
2022-04-18 09:47:48 -0400

    Login functionality restored and working with Azure Functions App.

--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const Connection = require("tedious").Connection;
 const Request = require("tedious").Request;
 const queries = require("./queries");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 // Creating Express app and setting port value
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Setting configuration for accessing hosted SQL DB with tedious
 const config = {
@@ -65,15 +65,13 @@ async function executeStatement(sql, cb) {
   connection.execSql(request);
 }
 
-var cors = require('cors')
-app.use(cors())
-
+var cors = require("cors");
+app.use(cors());
 
 // Client Side Routing
 app.use(express.static("build"));
 
 // This displays message that the server running and listening to specified port
-
 
 // Users
 app.get("/users", async (req, res) => {
@@ -131,30 +129,29 @@ app.get("/businesses", async (req, res) => {
   });
   return success;
 });
-app.post("/login",async (req,
-                        res) => {
-    const username = req.body.user.username;
-    const password = req.body.user.password;
-    console.log ("login:", username, " / ", password);
-    const select = `SELECT PasswordHash, Admin FROM AspNetUsers2 WHERE UserName = '${username}' `;
-    let success = await executeStatement(select, (rows) => {
-      let obj=[];
-      console.log ("ADMIN VALUE: ", rows[0].Admin);
-      if(rows.length===0 || rows[0].PasswordHash !== password)
-        obj={status: -1, message:"Invalid login", admin:false};
-      else
-       obj={status: 1, message:"User logged in", admin: rows[0].Admin};
+
+// Login POST Route
+app.post("/login", async (req, res) => {
+  const username = req.body.user.username;
+  const password = req.body.user.password;
+  console.log("login:", username, " / ", password);
+  const select = `SELECT PasswordHash, Admin FROM AspNetUsers2 WHERE UserName = '${username}' `;
+  let success = await executeStatement(select, (rows) => {
+    let obj = [];
+    console.log("ADMIN VALUE: ", rows[0].Admin);
+    if (rows.length === 0 || rows[0].PasswordHash !== password)
+      obj = { status: -1, message: "Invalid login", admin: false };
+    else obj = { status: 1, message: "User logged in", admin: rows[0].Admin };
 
     console.log("login result: ", obj);
     res.send(obj);
     //console.log(`Fetched ${rows.length} rows`);
-   // console.log(`Data: ${JSON.stringify(rows, null, 2)}`);
+    // console.log(`Data: ${JSON.stringify(rows, null, 2)}`);
+  });
+});
 
-    });
-})
-
-app.post("/registerNew", async (req,
-                                res) => {
+// Register new user POST Route
+app.post("/registerNew", async (req, res) => {
   console.log("body", req.body.user);
   const user = req.body.user;
   const insert = `INSERT INTO [dbo].[AspNetUsers2] (UserName, PasswordHash, Admin) VALUES('${user.regEmail}', '${user.createPass}', '0')`;
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,35 +1,35 @@
-import { React, useState } from 'react';
-import { Form, FormGroup, NavLink } from 'reactstrap';
-import { Col, Row } from 'reactstrap';
-import Dashboard from '../Dashboard';
-import AdminDashboard from '../AdminDashboard';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import './Login.css'; // Tell webpack that Button.js uses these styles
-import { Card } from '@mui/material';
-import dickerLogoSquare from '../images/dickerLogoSquare.png';
-import Paper from '@mui/material/Paper';
-import InputAdornment from '@mui/material/InputAdornment';
+import { React, useState } from "react";
+import { Form, FormGroup, NavLink } from "reactstrap";
+import { Col, Row } from "reactstrap";
+import Dashboard from "../Dashboard";
+import AdminDashboard from "../AdminDashboard";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import "./Login.css"; // Tell webpack that Button.js uses these styles
+import { Card } from "@mui/material";
+import dickerLogoSquare from "../images/dickerLogoSquare.png";
+import Paper from "@mui/material/Paper";
+import InputAdornment from "@mui/material/InputAdornment";
 import EmailIcon from "@mui/icons-material/Email";
 import axios from "axios";
 
 const Login = ({ userAuth, setUserAuth, isAdmin, setIsAdmin }) => {
   const [state, setState] = useState({
     data: null,
-    email: '',
-    password: '',
+    email: "",
+    password: "",
     auth: false,
     isAdmin: false,
     admin: {
-      name: 'Admin',
+      name: "Admin",
     },
     merchant: {
-      name: 'Bob',
+      name: "Bob",
     },
   });
 
@@ -39,96 +39,30 @@ const Login = ({ userAuth, setUserAuth, isAdmin, setIsAdmin }) => {
     let email = state.email;
     let password = state.password;
 
-    // call to server to authetnicat.
-
+    // call to server to authenticate.
     const user = {
       username: email,
-      password: password
-    };
-console.log ("logging in ", user);
-    axios.post('/login', {user})
-        .then(res => {
-          console.log("response from server");
-          console.log(res);
-          console.log("DATA: ", res.data);
-          const data = res.data;
-          if(data.status===1)
-          {
-            console.log ("USER IS AUTHENTICI");
-            if(data.admin)
-            {
-              console.log ("ADMIN USER");
-            }
-            setUserAuth(true);
-            setIsAdmin(data.admin);
-
-          }
-
-        });
-/*
-    const admin = {
-      id: 1,
-      email: 'admin@dicker.com',
-      password: 'admin',
-      name: 'Admin',
-      loginAttempts: 0,
-      lockoutEnabled: false,
-      isAdmin: true,
-    };
-    const merchant = {
-      id: 2,
-      email: 'firstcheck@lastcheck.test',
-      password: '12345',
-      name: 'Bob',
-      loginAttempts: 0,
-      lockoutEnabled: false,
-      isAdmin: false,
+      password: password,
     };
 
-    console.log(email + ' ' + password + ' ' + admin.email);
+    // Logging to track data capture
+    console.log("logging in ", user);
 
-    if (admin.email === email && admin.password === password) {
-      event.preventDefault();
-      console.log('login successful');
-      setUserAuth(true);
-      setIsAdmin(true);
-    }
-    if (admin.email === email && admin.password !== password) {
-      event.preventDefault();
-      admin.loginAttempts += 1;
-      console.log(admin.loginAttempts);
-      console.log('login failed');
-    }
-    if (merchant.email === email && merchant.password === password) {
-      event.preventDefault();
-      console.log('login successful');
-      setUserAuth(true);
-    }
-    if (merchant.email === email && merchant.password !== password) {
-      event.preventDefault();
-      merchant.loginAttempts += 1;
-      console.log(merchant.loginAttempts);
-      console.log('login failed');
-    }
-
-    if (merchant.email !== email && admin.email !== email) {
-      event.preventDefault();
-      console.log('login failed');
-    }
-
-    if (admin.loginAttempts === 3) {
-      event.preventDefault();
-      console.log("User locked out.");
-      admin.lockoutEnabled = true;
-    }
-
-    if (merchant.loginAttempts === 3) {
-      event.preventDefault();
-      console.log("User locked out.");
-      merchant.lockoutEnabled = true;
-    }
-    */
-
+    // Axios method to post login to server
+    axios.post("/api/login", { user }).then((res) => {
+      console.log(res);
+      const data = res.data[0];
+      console.log();
+      if (res.status === 200) {
+        console.log("USER IS AUTHENTIC"); // if response is good we have user
+        if (data.Admin) {
+          // if user admin property is true, we have admin
+          console.log("ADMIN USER");
+        }
+        setUserAuth(true);
+        setIsAdmin(data.Admin);
+      }
+    });
   };
 
   const handleChange = (event) => {
@@ -138,105 +72,128 @@ console.log ("logging in ", user);
 
   if (userAuth === false) {
     return (
-          <Card style={{ width: '500px', margin: 'auto', marginTop: '300px' }} sx={{ boxShadow: 3 }}>
-            <body id='loginBody'>
-              <Container className={'loginContainer'}>
-                {/* <TestComponent /> */}
-                <Paper
-                  sx={{ boxShadow: 0, marginLeft: '166px', marginRight: 'auto', marginTop: '15px', marginBottom: '15px' }}
-                >
-                  <img src={dickerLogoSquare} alt={'dicker logo'} />
-                </Paper>
-                <Form id='loginForm' onSubmit={login}>
-                  <FormGroup style={{ backgroundColor: '', alignContent: 'center' }}>
-                    <Row>
-                      <Col>
-                        <Typography fontWeight='bold' id='loginHeader' component='h1' variant='h5' align='center'>
-                          Sign in
-                        </Typography>
-                      </Col>
-                    </Row>
-                    <br />
-                    <Row>
-                      <Col>
-                        <TextField
-                          InputProps={{
-                            startAdornment: (
-                              <InputAdornment position='start'>
-                                <EmailIcon />
-                              </InputAdornment>
-                            ),
-                          }}
-                          type='email'
-                          name='email'
-                          required
-                          autoFocus
-                          margin='normal'
-                          label='Email Address'
-                          id='email'
-                          fullWidth
-                          value={state.email}
-                          onChange={handleChange}
-                          placeholder='Enter email...'
-                        />
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                        <TextField
-                          InputProps={{
-                            startAdornment: (
-                              <InputAdornment position='start'>
-                                <LockOutlinedIcon />
-                              </InputAdornment>
-                            ),
-                          }}
-                          type='password'
-                          name='password'
-                          id='password'
-                          required
-                          margin='normal'
-                          fullWidth
-                          label='Password'
-                          value={state.password}
-                          onChange={handleChange}
-                          placeholder='Enter password...'
-                        />
-                      </Col>
-                    </Row>
-                    <Row>
-                      <FormControlLabel
-                        sx={{ marginLeft: '6px' }}
-                        control={<Checkbox value='remember' color='primary' />}
-                        label='Remember me'
-                      />
-                    </Row>
-                    <Row style={{ marginTop: '-8px', alignContent: 'center' }}>
-                      <Col>
-                        <Button
-                          type='submit'
-                          fullWidth
-                          variant='contained'
-                          sx={{ backgroundColor: '#d9c07c', mt: 3, mb: 2 }}
-                        >
-                          Sign In
-                        </Button>
-                      </Col>
-                    </Row>
-                    <p className='App-intro'>{state.data}</p>
-                    <Row>
-                      <NavLink style={{ marginLeft: '85px' }} href='./resetPassword'>
-                        Forgot Password?
-                      </NavLink>
-                      <NavLink style={{ marginRight: 'auto' }} href='./CreateAccount'>
-                        Create Account
-                      </NavLink>
-                    </Row>
-                  </FormGroup>
-                </Form>
-              </Container>
-            </body>
-          </Card>
+      <Card
+        style={{ width: "500px", margin: "auto", marginTop: "300px" }}
+        sx={{ boxShadow: 3 }}
+      >
+        <body id="loginBody">
+          <Container className={"loginContainer"}>
+            {/* <TestComponent /> */}
+            <Paper
+              sx={{
+                boxShadow: 0,
+                marginLeft: "166px",
+                marginRight: "auto",
+                marginTop: "15px",
+                marginBottom: "15px",
+              }}
+            >
+              <img src={dickerLogoSquare} alt={"dicker logo"} />
+            </Paper>
+            <Form id="loginForm" onSubmit={login}>
+              <FormGroup
+                style={{ backgroundColor: "", alignContent: "center" }}
+              >
+                <Row>
+                  <Col>
+                    <Typography
+                      fontWeight="bold"
+                      id="loginHeader"
+                      component="h1"
+                      variant="h5"
+                      align="center"
+                    >
+                      Sign in
+                    </Typography>
+                  </Col>
+                </Row>
+                <br />
+                <Row>
+                  <Col>
+                    <TextField
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <EmailIcon />
+                          </InputAdornment>
+                        ),
+                      }}
+                      type="email"
+                      name="email"
+                      required
+                      autoFocus
+                      margin="normal"
+                      label="Email Address"
+                      id="email"
+                      fullWidth
+                      value={state.email}
+                      onChange={handleChange}
+                      placeholder="Enter email..."
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <TextField
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <LockOutlinedIcon />
+                          </InputAdornment>
+                        ),
+                      }}
+                      type="password"
+                      name="password"
+                      id="password"
+                      required
+                      margin="normal"
+                      fullWidth
+                      label="Password"
+                      value={state.password}
+                      onChange={handleChange}
+                      placeholder="Enter password..."
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <FormControlLabel
+                    sx={{ marginLeft: "6px" }}
+                    control={<Checkbox value="remember" color="primary" />}
+                    label="Remember me"
+                  />
+                </Row>
+                <Row style={{ marginTop: "-8px", alignContent: "center" }}>
+                  <Col>
+                    <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      sx={{ backgroundColor: "#d9c07c", mt: 3, mb: 2 }}
+                    >
+                      Sign In
+                    </Button>
+                  </Col>
+                </Row>
+                <p className="App-intro">{state.data}</p>
+                <Row>
+                  <NavLink
+                    style={{ marginLeft: "85px" }}
+                    href="./resetPassword"
+                  >
+                    Forgot Password?
+                  </NavLink>
+                  <NavLink
+                    style={{ marginRight: "auto" }}
+                    href="./CreateAccount"
+                  >
+                    Create Account
+                  </NavLink>
+                </Row>
+              </FormGroup>
+            </Form>
+          </Container>
+        </body>
+      </Card>
     );
   } else if (userAuth === true) {
     if (isAdmin === false) {
